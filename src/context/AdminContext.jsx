b499a0a2bd8201a20971f330177c232427ad951b
simonnjoroge <simonnjoroge806@gmail.com>
2025-09-22 23:05:30 +0300

    completed the all doctors page for the admin

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,16 +1,61 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
+
     const [adminAtoken, setAdminAtoken] = useState(localStorage.getItem('accessToken') || '');
+    const [doctors, setDoctors] = useState([]);
 
     //to access an environment variable in vite
     //use import.meta.env.variablename
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const value = {adminAtoken, setAdminAtoken, backendUrl} 
+    const getAllDoctors = async () => {
+        try {
+            
+            const { data } = await axios.get(backendUrl + '/api/admin/all-doctors', {}, {
+                headers:{
+                    'Authorization': `Bearer ${adminAtoken}`
+                }
+            });
+            if(data.success) {
+                setDoctors(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const toggleAvailabilty = async (docId) => {
+        try {
+        const { data } = await axios.post(backendUrl + '/api/admin/change-availability', {docId}, {
+            headers: {
+            'Authorization': `Bearer ${adminAtoken}`
+            }
+        })
+
+        if(data.success){
+            toast.success(data.message);
+            getAllDoctors();
+        } else {
+            toast.error(data.message)
+        }
+
+        } catch (error) {
+            //error happens on the client while 
+            //sending the request
+            toast.error(error.message)
+        }
+    }
+
+    const value = {adminAtoken, setAdminAtoken, backendUrl, doctors, getAllDoctors, toggleAvailabilty} 
 
     return (
         <AdminContext.Provider value={value}>
--- src/pages/admin/DoctorsList.jsx
+++ src/pages/admin/DoctorsList.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { useEffect } from 'react';
 
 const DoctorsList = () => {
+
+  const { adminAtoken, doctors, getAllDoctors, toggleAvailabilty } = useContext(AdminContext);
+
+ // console.log(doctors)
+
+  useEffect(()=>{
+    getAllDoctors()
+  }, [adminAtoken])
+
   return (
-    <div>DoctorsList</div>
+    <div className='m-5 max-h-[90vh] overflow-y-scroll'>
+      <h1 className='font-semibold text-3xl'>All Doctors</h1>
+      <div className='flex flex-wrap gap-4 pt-5 gap-y-6'>
+        {
+          doctors.map((doc, i)=>(
+            <div className='border border-indigo-200 rounded-xl max-w-[56]
+             overflow-hidden cursor-pointer group' key={i}
+            >
+              <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-500' src={doc.image} alt="" />
+              <div className='p-4'>
+                <p className='text-neutral-800 text-lg font-medium'>{doc.name}</p>
+                <p className='text-zinc-600 text-sm'>{doc.speciality}</p>
+                <div className='mt-4 flex items-center text-sm'>
+                  <input onClick={()=>toggleAvailabilty(doc._id)} type='checkbox' checked={doc.available}/>
+                  <p>available</p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
