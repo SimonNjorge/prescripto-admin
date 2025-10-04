e1b4a9a9c3561d168c5bd4b22cc4ec12018a0ce5
simonnjoroge <simonnjoroge806@gmail.com>
2025-10-04 12:48:10 +0300

    added routes for doctor panel

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,33 @@ import DoctorsList from './pages/admin/DoctorsList';
 import AllAppointments from './pages/admin/AllAppointments';
 import Dashboard from './pages/admin/Dashboard';
 import AddDoctor from './pages/admin/AddDoctor';
+import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/doctor/DoctorDashboard';
+import DoctorProfile from './pages/doctor/DoctorProfile';
+import DoctorAppointments from './pages/doctor/DoctorAppointments';
 
 const App = () => {
 
   const {adminAtoken} = useContext(AdminContext);
+  const {docAtoken} = useContext(DoctorContext);
 
-  return adminAtoken ? (
+  return adminAtoken || docAtoken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <Navbar/>
       <div className='flex items-start'>
         <Sidebar/>
         <Routes>
+          {/* Admin routes*/}
           <Route path='/' element={<></>}/>
           <Route path='/doctors-list' element={<DoctorsList/>}/>
           <Route path='/add-doctor' element={<AddDoctor/>}/>
           <Route path='/all-appointments' element={<AllAppointments/>}/>
           <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          {/*Doctor routes*/}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
+          <Route path='/doctor-profile' element={<DoctorProfile/>}/>
+          <Route path='/doctor-appointments' element={<DoctorAppointments/>}/>
         </Routes>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React, { useContext, useLayoutEffect } from 'react'
+import React, { useContext} from 'react'
 import { NavLink } from 'react-router-dom'
 import { AdminContext } from '../context/AdminContext'
 import { assets } from '../assets/assets';
+import { DoctorContext } from '../context/DoctorContext';
 
 const Sidebar = () => {
     const {adminAtoken} = useContext(AdminContext);
+    const {docAtoken} = useContext(DoctorContext);
 
   return (
     <div className='min-h-screen bg-white border-gray-300 border-r'>
@@ -28,7 +30,24 @@ const Sidebar = () => {
                 <img src={assets.people_icon} alt="" />
                 <p>Doctors List</p>
             </NavLink>
-        </ul>
+         </ul>
+        }
+        {docAtoken && <ul className='text-gray-600 mt-5'>
+            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-indigo-100 border-r-4 border-primary' : ''}`} to='/doctor-dashboard' >
+                <img src={assets.home_icon} alt="" />
+                <p>Dashboard</p>
+            </NavLink>
+
+            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-indigo-100 border-r-4 border-primary' : ''}`} to='/doctor-appointments'>
+                <img src={assets.appointment_icon} alt="" />
+                <p>Appointments</p>
+            </NavLink>
+
+            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-indigo-100 border-r-4 border-primary' : ''}`} to='/doctor-profile'>
+                <img src={assets.people_icon} alt="" />
+                <p>Profile</p>
+            </NavLink>
+         </ul>
         }
     </div>
   )
