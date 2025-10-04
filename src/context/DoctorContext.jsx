8e8ed26b9443ba4a351ac5e68f62b7fbe33c5ce7
simonnjoroge <simonnjoroge806@gmail.com>
2025-10-04 12:53:14 +0300

    added api to fetch doc appointments for doc panel

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -1,9 +1,39 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-    const value = {}
+
+    const [appointments, setAppointments] = useState([]);
+    const [docAtoken, setDocAtoken] = useState(localStorage.getItem('docAtoken') || '');
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    const getMyAppointments = async () => {
+        try {
+            
+            const { data } = await axios.post(backendUrl + '/api/doctor/get-myAppointments', {},
+                {headers: {'Authorization' : `Bearer ${docAtoken}`}}
+            );
+            if (data.success) {
+                setAppointments(data.appointments);
+                console.log(data.appointments)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const value = {
+        docAtoken, setDocAtoken,
+        appointments, getMyAppointments,
+        backendUrl
+    }
+
     return (
         <DoctorContext.Provider value={value}>
             {props.children}
@@ -11,4 +41,4 @@ const DoctorContextProvider = (props) => {
     )
 }
 
-export default DoctorContextProvider
+export { DoctorContextProvider, DoctorContext}
